4fd74c623b3608565240629ceba8d10a87f6717c
Твоё имя < email  GitHub>
2025-10-02 15:26:11 +0300

    DM-1002 add iocns and added cards text

--- src/settings/Dent-Life.ts
+++ src/settings/Dent-Life.ts
@@ -1,5 +1,8 @@
+
 interface metaInfo {
   name: string;
+  address: string;
+  schedule: string;
   description: string;
   keywords: string; // SEO keywords
   banner: {
@@ -7,8 +10,12 @@ interface metaInfo {
     doctor_name: string;
     doctor_title: string;
   };
-  address: string;
-  schedule: string;
+  services?: {
+    id: number;
+    title: string;
+    image: string;
+    details: { title: string }[];
+  }[];
 }
 export const META: Record<'ua' | 'ru' | 'en', metaInfo> = {
   ua: {
@@ -21,23 +28,246 @@ export const META: Record<'ua' | 'ru' | 'en', metaInfo> = {
       "стоматологія, клініка, Dent-Life, лікування зубів, профілактика, посмішка, здоров'я ротової порожнини", // SEO keywords
     banner: {
       text: 'Стоматологія - наука, переважно прикладна, тому хороший стоматолог - це, як талановитий художник. Насправді неважливо які у лікаря дипломи та регалії, а важливо які у нього руки.',
-      doctor_name: 'Максименко Олег В\'ячеславович',
+      doctor_name: "Максименко Олег В'ячеславович",
       doctor_title: 'лікар-стоматолог, терапевт',
-    }
+    },
+    services: [
+      {
+        id: 1,
+        title: 'Чищення зубів',
+        image: 'dl-tooth-filling.svg',
+        details: [
+          {
+            title: 'Професійне чищення зубів',
+          },
+          {
+            title: 'Видалення зубного нальоту ультразвуком',
+          },
+          {
+            title: 'Видалення зубного каменю',
+          },
+          {
+            title: 'Чищення зубів Air Flow',
+          },
+          {
+            title: 'Відбілювання зубів',
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: 'Лікування зубів',
+        image: 'dl-care.svg',
+        details: [
+          {
+            title: 'Лікування карієсу',
+          },
+          {
+            title: 'Лікування пульпіту',
+          },
+          {
+            title: 'Лікування періодонтиту',
+          },
+          {
+            title: 'Реставрація зубів',
+          },
+          {
+            title: 'Лікування коренів зубів',
+          },
+          {
+            title: 'Лікування ясен',
+          },
+          {
+            title: 'Лікування зубів під наркозом',
+          },
+          { title: 'Видалення зубів' },
+        ],
+      },
+      {
+        id: 3,
+        title: 'Протезування зубів',
+        image: 'dl-koronka.svg',
+        details: [
+          {
+            title: 'Встановлення коронок',
+          },
+          {
+            title: 'Цирконієві коронки',
+          },
+          {
+            title: 'Коронки на імплантах',
+          },
+          { title: 'Вініри' },
+          {
+            title: 'Металокерамічні коронки',
+          },
+        ],
+      },
+      {
+        id: 4,
+        title: 'Імплантація зубів',
+        image: 'dl-implant.svg',
+        details: [
+          {
+            title: 'Встановлення зубних імплантів',
+          },
+          {
+            title: 'Імплантація All-on-4',
+          },
+          {
+            title: 'Синус-ліфтинг',
+          },
+          {
+            title: 'Кісткова пластика',
+          },
+        ],
+      },
+      {
+        id: 5,
+        title: 'Рентген діагностика',
+        image: 'dl-x-ray.svg',
+        details: [
+          { title: 'Прицільний знімок' },
+          { title: 'Ортопантомограма (панорамний знімок)' },
+        ],
+      },
+      {
+        id: 6,
+        title: "Дитяча стоматологія",
+        image: 'dl-happy-tooth.svg',
+        details: [
+          { title: 'Лікування молочних зубів' },
+          { title: 'Профілактика карієсу у дітей' },
+          { title: 'Фторування зубів' },
+          { title: 'Герметизація фісур' },
+        ],
+      },
+    ],
   },
   ru: {
     name: 'Дент-Лайф',
     address: 'г. Вишневое, ул. Витянская, 1-А, офис 1',
     schedule: 'Пн-Сб 9:00-19:00 <br> Вс - выходной',
-    description:
-      'Стоматологическая клиника Dent-Life - современный подход к вашему здоровью и улыбке.',
+    description: 'Стоматологическая клиника Dent-Life - современный подход к вашему здоровью и улыбке.',
     keywords:
       'стоматология, клиника, Dent-Life, лечение зубов, профилактика, улыбка, здоровье ротовой полости', // SEO keywords
     banner: {
-      text: 'Стоматология - наука, по большей части прикладная, поэтому хороший стоматолог - это, как талантливый художник. На самом деле неважно какие у врача дипломы и регалии, а важно какие у него руки.',
+      text: 'Стоматология - наука, преимущественно прикладная, поэтому хороший стоматолог - это, как талантливый художник. На самом деле неважно какие у врача дипломы и регалии, а важно какие у него руки.',
       doctor_name: 'Максименко Олег Вячеславович',
       doctor_title: 'врач-стоматолог, терапевт',
-    }
+    },
+    services: [
+      {
+        id: 1,
+        title: 'Чистка зубов',
+        image: 'dl-tooth-filling.svg',
+        details: [
+          {
+            title: 'Профессиональная чистка зубов',
+          },
+          {
+            title: 'Удаление зубного налета ультразвуком',
+          },
+          {
+            title: 'Удаление зубного камня',
+          },
+          {
+            title: 'Чистка зубов Air Flow',
+          },
+          {
+            title: 'Отбеливание зубов',
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: 'Лечение зубов',
+        image: 'dl-care.svg',
+        details: [
+          {
+            title: 'Лечение кариеса',
+          },
+          {
+            title: 'Лечение пульпита',
+          },
+          {
+            title: 'Лечение периодонтита',
+          },
+          {
+            title: 'Реставрация зубов',
+          },
+          {
+            title: 'Лечение корней зубов',
+          },
+          {
+            title: 'Лечение десен',
+          },
+          {
+            title: 'Лечение зубов под наркозом',
+          },
+          { title: 'Удаление зубов' },
+        ],
+      },
+      {
+        id: 3,
+        title: 'Протезирование зубов',
+        image: 'dl-koronka.svg',
+        details: [
+          {
+            title: 'Установка коронок',
+          },
+          {
+            title: 'Циркониевые коронки',
+          },
+          {
+            title: 'Коронки на имплантах',
+          },
+          { title: 'Виниры' },
+          {
+            title: 'Металлокерамические коронки',
+          },
+        ],
+      },
+      {
+        id: 4,
+        title: 'Имплантация зубов',
+        image: 'dl-implant.svg',
+        details: [
+          {
+            title: 'Установка зубных имплантов',
+          },
+          {
+            title: 'Имплантация All-on-4',
+          },
+          {
+            title: 'Синус-лифтинг',
+          },
+          {
+            title: 'Костная пластика',
+          },
+        ],
+      },
+      {
+        id: 5,
+        title: 'Рентген диагностика',
+        image: 'dl-x-ray.svg',
+        details: [
+          { title: 'Прицельный снимок' },
+          { title: 'Ортопантомограмма (панорамный снимок)' },
+        ],
+      },
+      {
+        id: 6,
+        title: 'Детская стоматология',
+        image: 'dl-happy-tooth.svg',
+        details: [
+          { title: 'Лечение молочных зубов' },
+          { title: 'Профилактика кариеса у детей' },
+          { title: 'Фторирование зубов' },
+          { title: 'Герметизация фиссур' },
+        ],
+      },
+    ],
   },
   en: {
     name: 'Dent-Life',
@@ -49,9 +279,117 @@ export const META: Record<'ua' | 'ru' | 'en', metaInfo> = {
       text: 'Dentistry is a science, mostly applied, so a good dentist is like a talented artist. In fact, it does not matter what diplomas and regalia the doctor has, but what kind of hands he has.',
       doctor_name: 'Maksymenko Oleg Vyacheslavovich',
       doctor_title: 'dentist-therapist',
-    }
+    },
+    services: [
+      {
+        id: 1,
+        title: 'Teeth cleaning',
+        image: 'dl-tooth-filling.svg',
+        details: [
+          {
+            title: 'Professional teeth cleaning',
+          },
+          {
+            title: 'Removal of dental plaque with ultrasound',
+          },
+          {
+            title: 'Removal of tartar',
+          },
+          {
+            title: 'Air Flow teeth cleaning',
+          },
+          {
+            title: 'Teeth whitening',
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: 'Dental treatment',
+        image: 'dl-care.svg',
+        details: [
+          {
+            title: 'Caries treatment',
+          },
+          {
+            title: 'Pulpitis treatment',
+          },
+          {
+            title: 'Periodontitis treatment',
+          },
+          {
+            title: 'Tooth restoration',
+          },
+          {
+            title: 'Root canal treatment',
+          },
+          {
+            title: 'Gum treatment',           },
+        ],
+      },
+      {
+        id: 3,
+        title: 'Dental prosthetics',
+        image: 'dl-koronka.svg',
+        details: [
+          {
+            title: 'Crown installation',
+          },
+          {
+            title: 'Zirconia crowns',
+          },
+          {
+            title: 'Crowns on implants',
+          },
+          { title: 'Veneers' },
+          {
+            title: 'Metal-ceramic crowns',
+          },
+        ],
+      },
+      {
+        id: 4,
+        title: 'Dental implantation',
+        image: 'dl-implant.svg',
+        details: [
+          {
+            title: 'Dental implant placement',
+          },
+          {
+            title: 'All-on-4 implantation',
+          },
+          {
+            title: 'Sinus lift',
+          },
+          {
+            title: 'Bone grafting',
+          },
+        ],
+      },
+      {
+        id: 5,
+        title: 'X-ray diagnostics',
+        image: 'dl-x-ray.svg',
+        details: [
+          { title: 'Targeted image' },
+          { title: 'Orthopantomogram (panoramic image)' },
+        ],
+      },
+      {
+        id: 6,
+        title: 'Pediatric dentistry',
+        image: 'dl-happy-tooth.svg',
+        details: [
+          { title: 'Treatment of baby teeth' },
+          { title: 'Prevention of caries in children' },
+          { title: 'Tooth fluoridation' },
+          { title: 'Fissure sealing' },
+        ],
+      },
+    ],
   },
 };
+
 interface contactInfo {
   phone_1: string;
   phone_2: string;
