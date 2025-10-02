53495327557ee11113dfb3fd10024473a245b976
Твоё имя < email  GitHub>
2025-10-02 11:55:12 +0300

    DM-1002 add footer

--- src/App.vue
+++ src/App.vue
@@ -2,89 +2,37 @@
 import { RouterLink, RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
 import OfficeGeo from '@/components/OfficeGeo.vue';
+import SiteBanner from '@/components/SiteBanner.vue';
+import DentLifeHeader from '@/components/DentLifeHeader.vue';
+import DentLifeFooter from '@/components/DentLifeFooter.vue';
+
 </script>
 
 <template>
-  <v-app>
-    <v-app-bar>
-            <v-container class="dl-container mx-auto" max-width="1280" center>jsadfhkSADHFK</v-container>
-    </v-app-bar>
+  <v-app class="dent-life-app">
+    <DentLifeHeader />
     <v-main>
-      <v-container>
+      <v-container max-width="1280">
+        <SiteBanner />
         <h1>Main Content</h1>
         <HelloWorld msg="You did it!" />
-
         <nav>
           <RouterLink to="/">Home</RouterLink>
           <RouterLink to="/about">About</RouterLink>
         </nav>
-
         <RouterView />
-
         <OfficeGeo />
       </v-container>
     </v-main>
+    <DentLifeFooter />
   </v-app>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.dent-life-app {
+  outline: 1px solid rgba(11, 88, 221, 0.393);
+}
 .dl-container {
-  outline: 1px solid rgba(205, 50, 122, 0.413);
-}
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  outline: 1px solid rgba(221, 11, 88, 0.393);
 }
 </style>
