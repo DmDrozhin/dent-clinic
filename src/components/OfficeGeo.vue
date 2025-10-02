5ab3feb6828f3989e53fbcd5ad8b3dbe1d3e12e3
Твоё имя < email  GitHub>
2025-10-02 11:56:02 +0300

    DM-1002 corrected google map component

--- src/components/OfficeGeo.vue
+++ src/components/OfficeGeo.vue
@@ -1,43 +1,43 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 import { GoogleMap, AdvancedMarker, InfoWindow } from 'vue3-google-map';
 import { createAssetMap } from '@/utils/assets.ts';
 import ContactsBlock from '@/components/ContactsBlock.vue';
+import { useMaineStore } from '@/stores/main.store.ts';
 
 interface Props {
   options?: Record<string, unknown>;
 }
 const props = withDefaults(defineProps<Props>(), {
-  options: () => ({}), // дефолтное значение
+  options: () => ({}),
+});
+
+const store = useMaineStore();
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const selectedLanguage = computed(() => {
+  const lang = store.currentLang?.toLowerCase();
+  if (lang === 'ua') return 'uk';
+  return ['uk', 'en', 'ru'].includes(lang || '') ? lang : 'uk';
 });
 
 const mapKey = props.options.apiKey || import.meta.env.VITE_GOOGLE_API_KEY;
 const mapId = props.options.apiKey || import.meta.env.VITE_GOOGLE_GRAY_MAP_ID;
 
-// выбранный язык (по умолчанию украинский)
-const selectedLanguage = ref('uk');
 const region = ref('UA');
-
-const languageOptions = [
-  { title: 'Українська', value: 'uk', name: 'Дент-Лайф' },
-  { title: 'English', value: 'en', name: 'Dent-Life' },
-  { title: 'Русский', value: 'ru', name: 'Дент-Лайф' },
-];
-
 const center = { lat: 50.3891, lng: 30.382977 };
 const zoom = ref(17);
-const markerOptions = {
+const markerOptions = computed(() => ({
   position: center,
-  title: 'Dent-Life',
-};
+  title: store.currentMeta?.name || 'Дент-Лайф',
+}));
 
 const mapRef = ref<InstanceType<typeof GoogleMap> | null>(null);
+watchEffect(() => {
+  if (mapRef.value && mapRef.value.map) {
+    console.log('Map instance:', mapRef.value.ready);
+  }
+});
 
-const onMapLoaded = (map: google.maps.Map) => {
-  console.log('Map loaded with language:', selectedLanguage.value);
-  console.log('API Key used:', mapKey ? '✅ Loaded' : '❌ Missing');
-  console.log('Map instance:', map);
-};
 const infoWindow = ref(true); // Will be open when mounted
 
 const userIcons = import.meta.glob<{ default: string }>('@/assets/images/ui/*', { eager: true });
@@ -46,37 +46,32 @@ const userIconsMap = createAssetMap(userIcons);
 
 <template>
   <div class="map-block">
-    <v-card class="mb-4">
-      <v-select v-model="selectedLanguage" :items="languageOptions" label="Map Language" dense />
-    </v-card>
     <v-alert v-if="!mapKey" type="error" class="mb-4"> Google Maps API Key is missing! </v-alert>
     <v-card>
       <GoogleMap
-        :key="selectedLanguage"
         :api-key="mapKey"
         :map-id="mapId"
-        :language="selectedLanguage"
+        language="uk"
         :region="region"
         :center="center"
         :zoom="zoom"
-        @loaded="onMapLoaded"
         style="width: 100%; height: 500px"
         ref="mapRef"
         class="map-block__google-map"
       >
-        <AdvancedMarker class="marker" :options="{ position: markerOptions.position }">
+        <AdvancedMarker class="advanced-marker" :options="{ position: markerOptions.position }">
           <template #content>
-            <div class="marker__sign">
+            <div class="dent-life-sign">
               <v-img
-                class="marker__sign-image"
+                class="dent-life-sign__image"
                 :src="userIconsMap['logo-white.svg']"
-                max-width="16"
+                width="12"
                 alt="logo"
               />
-              <span class="marker__sign-text">{{ markerOptions.title }}</span>
+              <span class="dent-life-sign__text">{{ markerOptions.title }}</span>
             </div>
           </template>
-          <InfoWindow v-model="infoWindow" class="marker__info">
+          <InfoWindow v-model="infoWindow" class="advanced-marker__info">
             <ContactsBlock />
           </InfoWindow>
         </AdvancedMarker>
@@ -86,15 +81,16 @@ const userIconsMap = createAssetMap(userIcons);
 </template>
 <style lang="scss" scoped>
 .map-block {
-  .marker {
-    &__sign {
-      $sign-color: green;
+  .advanced-marker {
+    .dent-life-sign {
+      $sign-color: rgb(var(--v-theme-secondary));
       width: 100%;
-      display: flex;
+      display: grid;
+      grid-template-columns: 1fr auto;
       align-items: center;
       gap: 4px;
       background-color: $sign-color;
-      padding: 4px 8px;
+      padding: 8px 12px;
       border-radius: 4px;
       position: relative;
       &::before {
@@ -108,19 +104,18 @@ const userIconsMap = createAssetMap(userIcons);
         left: 50%;
         transform: translateX(-50%);
       }
-    }
-    &__sign-image {
-      width: 100%;
-      flex-shrink: 0;
-    }
-    &__sign-text {
-      flex-shrink: 0;
-      color: white;
-      letter-spacing: 2px;
-      font-weight: 500;
-    }
-    &__info {
-      padding-top: 24px;
+      &__image {
+        width: 100%;
+        flex-shrink: 0;
+      }
+      &__text {
+        font-size: 0.75rem;
+        flex-shrink: 0;
+        letter-spacing: 1.5px;
+        font-weight: 500;
+        color: $white;
+        line-height: normal;
+      }
     }
   }
 }
@@ -129,7 +124,7 @@ const userIconsMap = createAssetMap(userIcons);
 .map-block {
   &__google-map {
     .gm-style-iw.gm-style-iw-c {
-      min-width: 260px !important;
+      min-width: 295px !important;
     }
     .gm-style-iw-chr {
       position: relative;
@@ -142,8 +137,17 @@ const userIconsMap = createAssetMap(userIcons);
       right: 0;
       width: 32px !important;
       height: 32px !important;
+      display: grid !important;
+      place-content: center;
       & > span {
         margin: 0 !important;
+        width: 20px !important;
+        height: 20px !important;
+      }
+    }
+    .advanced-marker {
+      &__info {
+        padding-top: 24px;
       }
     }
   }
