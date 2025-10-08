e9c787e24b4f12b984790e560839948f4eb3ac29
Твоё имя < email  GitHub>
2025-10-08 16:32:25 +0300

    MAIN refactor contacts block added picture

--- src/components/ContactsBlock.vue
+++ src/components/ContactsBlock.vue
@@ -1,104 +1,170 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { CONTACTS } from '@/settings/Dent-Life.ts';
-import { useMaineStore } from '@/stores/main.store.ts';
-import DentLifeLogo from '@/components/DentLifeLogo.vue';
-import { formatPhone } from '@/utils/assets.ts';
+  import { computed } from 'vue';
+  import { CONTACTS } from '@/settings/Dent-Life.ts';
+  import { useMaineStore } from '@/stores/main.store.ts';
+  // import DentLifeLogo from '@/components/DentLifeLogo.vue';
+  import { formatPhone } from '@/utils/assets.ts';
+  import { getImagePath } from '@/utils/assets.ts';
+  import { useDisplay } from 'vuetify';
 
-const store = useMaineStore();
-interface Props {
-  options?: Record<string, unknown>;
-}
+  const entrance = getImagePath('entrance.webp');
+  const { mdAndUp } = useDisplay();
 
-const props = withDefaults(defineProps<Props>(), {
-  options: () => ({}),
-});
+  const store = useMaineStore();
+  interface Props {
+    options?: Record<string, unknown>;
+  }
 
-const currentMeta = computed(() => store.currentMeta || {});
+  const props = withDefaults(defineProps<Props>(), {
+    options: () => ({})
+  });
 
-const defaultOptions = {};
-const mainOptions = computed(() => ({
-  ...defaultOptions,
-  ...props.options,
-  ...currentMeta.value,
-  ...CONTACTS,
-}));
+  const currentMeta = computed(() => store.currentMeta || {});
 
-const formattedPhones = computed(() => ({
-  phone1: formatPhone(mainOptions.value.phone_1),
-  phone2: formatPhone(mainOptions.value.phone_2),
-  viber: formatPhone(mainOptions.value.viber),
-}));
+  const defaultOptions = {};
+  const mainOptions = computed(() => ({
+    ...defaultOptions,
+    ...props.options,
+    ...currentMeta.value,
+    ...CONTACTS
+  }));
+
+  const formattedPhones = computed(() => ({
+    phone1: formatPhone(mainOptions.value.phone_1),
+    phone2: formatPhone(mainOptions.value.phone_2),
+    viber: formatPhone(mainOptions.value.viber)
+  }));
 </script>
 
 <template>
   <div class="contacts">
-    <DentLifeLogo
-      :options="{ mainHeader: false, maxWidth: 32, light: false, customClass: 'dl-center' }"
-    />
-    <v-divider class="my-3" />
-    <div class="contacts__content">
-      <div v-if="mainOptions.address" class="contacts__wrapper meta address">
-        <v-img :src="store.userIconsMap['meta-geo.svg']" max-width="20" />
-        <div class="contacts__address" v-html="mainOptions.address"></div>
-      </div>
-      <div v-if="mainOptions.phone_1 || mainOptions.phone_2" class="contacts__wrapper meta">
-        <v-img :src="store.userIconsMap['meta-phone.svg']" max-width="20" />
-        <div class="contacts__phones">
-          <span v-if="mainOptions.phone_1">
-            <a :href="`tel:${mainOptions.phone_1}`">{{ formattedPhones.phone1 }}</a>
-          </span>
-          <span v-if="mainOptions.phone_2">
-            <a :href="`tel:${mainOptions.phone_2}`">{{ formattedPhones.phone2 }}</a>
-          </span>
+    <div class="contacts__left-side">
+      <v-img
+        class="contacts__outdoor-pic"
+        :src="entrance"
+        absolute
+        cover
+        alt="dent-life-outdoor" />
+      <!-- <DentLifeLogo :options="{ mainHeader: false, maxWidth: 32, light: false, customClass: 'dl-center' }" /> -->
+    </div>
+    <div
+      class="contacts__right-side"
+      v-if="mdAndUp">
+      <div class="contacts__content">
+        <div
+          v-if="mainOptions.address"
+          class="contacts__wrapper meta address">
+          <v-img
+            class="contacts__icon"
+            :src="store.userIconsMap['meta-geo.svg']"
+            max-width="20" />
+          <div
+            class="contacts__address"
+            v-html="mainOptions.address"></div>
         </div>
-      </div>
-      <div class="contacts__wrapper meta" v-if="mainOptions.viber">
-        <v-img :src="store.userIconsMap['meta-viber.svg']" max-width="20" />
-        <div class="contacts__viber">
-          <a :href="`viber://chat?number=${mainOptions.viber}`"
-            >Viber: {{ formattedPhones.viber }}</a
-          >
+        <div
+          v-if="mainOptions.phone_1 || mainOptions.phone_2"
+          class="contacts__wrapper meta">
+          <v-img
+            class="contacts__icon"
+            :src="store.userIconsMap['meta-phone.svg']"
+            max-width="20" />
+          <div class="contacts__phones">
+            <span v-if="mainOptions.phone_1">
+              <a :href="`tel:${mainOptions.phone_1}`">{{ formattedPhones.phone1 }}</a>
+            </span>
+            <span v-if="mainOptions.phone_2">
+              <a :href="`tel:${mainOptions.phone_2}`">{{ formattedPhones.phone2 }}</a>
+            </span>
+          </div>
         </div>
-      </div>
+        <div
+          class="contacts__wrapper meta"
+          v-if="mainOptions.viber">
+          <v-img
+            class="contacts__icon"
+            :src="store.userIconsMap['meta-viber.svg']"
+            max-width="20" />
+          <div class="contacts__viber">
+            <a :href="`viber://chat?number=${mainOptions.viber}`">Viber: {{ formattedPhones.viber }}</a>
+          </div>
+        </div>
 
-      <div v-if="mainOptions.email" class="contacts__wrapper meta">
-        <v-img :src="store.userIconsMap['meta-email.svg']" max-width="18" />
-        <div class="contacts__email">
-          <a :href="`mailto:${mainOptions.email}`">{{ mainOptions.email }}</a>
+        <div
+          v-if="mainOptions.email"
+          class="contacts__wrapper meta">
+          <v-img
+            class="contacts__icon"
+            :src="store.userIconsMap['meta-email.svg']"
+            max-width="18" />
+          <div class="contacts__email">
+            <a :href="`mailto:${mainOptions.email}`">{{ mainOptions.email }}</a>
+          </div>
         </div>
-      </div>
-      <div v-if="mainOptions.schedule" class="contacts__wrapper meta schedule">
-        <v-img :src="store.userIconsMap['meta-schedule.svg']" max-width="20" />
-        <div class="contacts__schedule" v-html="mainOptions.schedule"></div>
+        <div
+          v-if="mainOptions.schedule"
+          class="contacts__wrapper meta schedule">
+          <v-img
+            class="contacts__icon"
+            :src="store.userIconsMap['meta-schedule.svg']"
+            max-width="20" />
+          <div
+            class="contacts__schedule"
+            v-html="mainOptions.schedule"></div>
+        </div>
       </div>
     </div>
   </div>
+  <!-- <v-divider class="my-3" /> -->
 </template>
 
 <style lang="scss" scoped>
-.contacts {
-  width: 100%;
-  min-height: 50px;
-  &__content {
-    position: relative;
-    font-weight: 500;
-  }
-  &__wrapper {
+  @use '@/styles/settings.scss';
+
+  .contacts {
+    width: 100%;
+    min-height: 50px;
     display: flex;
-    align-items: center;
-    gap: 8px;
-    margin-bottom: 8px;
-    &.meta * {
-      font-family: 'jost', sans-serif;
-      color: black;
-      gap: 12px;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 12px;
+    &__left-side {
+      position: relative;
+      min-width: 320px;
+      aspect-ratio: 16/10;
+      @include settings.respond-down(sm) {
+        min-width: 260px;
+      }
+    }
+    &__right-side {
+      flex-shrink: 0;
+    }
+    &__outdoor-pic {
+      position: absolute;
+    }
+    &__content {
+      position: relative;
+      font-weight: 500;
+    }
+    &__wrapper {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      margin-bottom: 8px;
+      &.meta * {
+        font-family: 'jost', sans-serif;
+        color: black;
+        gap: 12px;
+      }
+    }
+    &__icon {
+      flex-shrink: 0;
+      width: 100%;
+    }
+    &__address,
+    &__schedule {
+      padding: 2px 4px;
     }
   }
-  &__address,
-  &__schedule {
-    padding: 2px 4px;
-  }
-}
 </style>
 <style lang="scss"></style>
--- src/components/OfficeGeo.vue
+++ src/components/OfficeGeo.vue
@@ -1,64 +1,69 @@
 <script setup lang="ts">
-import { computed, ref, watchEffect } from 'vue';
-import { GoogleMap, AdvancedMarker, InfoWindow } from 'vue3-google-map';
-import { createAssetMap } from '@/utils/assets.ts';
-import ContactsBlock from '@/components/ContactsBlock.vue';
-import { useMaineStore } from '@/stores/main.store.ts';
+  import { computed, ref, watchEffect } from 'vue';
+  import { GoogleMap, AdvancedMarker, InfoWindow } from 'vue3-google-map';
+  import { createAssetMap } from '@/utils/assets.ts';
+  import ContactsBlock from '@/components/ContactsBlock.vue';
+  import { useMaineStore } from '@/stores/main.store.ts';
 
-interface Props {
-  options?: Record<string, unknown>;
-}
-withDefaults(defineProps<Props>(), {
-  options: () => ({}),
-});
-
-const store = useMaineStore();
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const selectedLanguage = computed(() => {
-  const lang = store.currentLang?.toLowerCase();
-  if (lang === 'ua') return 'uk';
-  return ['uk', 'en', 'ru'].includes(lang || '') ? lang : 'uk';
-});
-
-const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
-const styleKey = import.meta.env.VITE_GOOGLE_GRAY_MAP;
-
-const region = ref('UA');
-const center = { lat: 50.3891, lng: 30.382977 };
-const zoom = ref(17);
-const markerOptions = computed(() => ({
-  position: center,
-  title: store.currentMeta?.name || 'Дент-Лайф',
-}));
-
-const mapRef = ref<InstanceType<typeof GoogleMap> | null>(null);
-watchEffect(() => {
-  if (mapRef.value && mapRef.value.map) {
-    console.log('Map instance:', mapRef.value.ready);
+  interface Props {
+    options?: Record<string, unknown>;
   }
-});
+  withDefaults(defineProps<Props>(), {
+    options: () => ({})
+  });
 
-const infoWindow = ref(true); // Will be open when mounted
+  const store = useMaineStore();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const selectedLanguage = computed(() => {
+    const lang = store.currentLang?.toLowerCase();
+    if (lang === 'ua') return 'uk';
+    return ['uk', 'en', 'ru'].includes(lang || '') ? lang : 'uk';
+  });
 
-const userIcons = import.meta.glob<{ default: string }>('@/assets/images/ui/*', { eager: true });
-const userIconsMap = createAssetMap(userIcons);
+  const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+  const styleKey = import.meta.env.VITE_GOOGLE_GRAY_MAP;
 
-watchEffect(() => {
-  if (mapRef.value?.ready && mapRef.value.map) {
-    mapRef.value.map.setOptions({
-      streetViewControl: false, // убираем Pegman
-      mapTypeControl: false,
-      fullscreenControl: false,
-      zoomControl: true, // оставляем масштаб
-      disableDefaultUI: false, // отключает все стандартные кнопки (если true)
-    });
-  }
-});
+  const region = ref('UA');
+  const center = { lat: 50.3891, lng: 30.382977 };
+  const zoom = ref(17);
+  const markerOptions = computed(() => ({
+    position: center,
+    title: store.currentMeta?.name || 'Дент-Лайф'
+  }));
+
+  const mapRef = ref<InstanceType<typeof GoogleMap> | null>(null);
+  watchEffect(() => {
+    if (mapRef.value && mapRef.value.map) {
+      console.log('Map instance:', mapRef.value.ready);
+    }
+  });
+
+  const infoWindow = ref(true); // Will be open when mounted
+
+  const userIcons = import.meta.glob<{ default: string }>('@/assets/images/ui/*', { eager: true });
+  const userIconsMap = createAssetMap(userIcons);
+
+  watchEffect(() => {
+    if (mapRef.value?.ready && mapRef.value.map) {
+      mapRef.value.map.setOptions({
+        streetViewControl: false, // убираем Pegman
+        mapTypeControl: false,
+        fullscreenControl: false,
+        zoomControl: true, // оставляем масштаб
+        disableDefaultUI: false // отключает все стандартные кнопки (если true)
+      });
+    }
+  });
 </script>
 
 <template>
   <div class="map-block">
-    <v-alert v-if="!apiKey" type="error" class="mb-4"> Google Maps API Key is missing! </v-alert>
+    <v-alert
+      v-if="!apiKey"
+      type="error"
+      class="mb-4">
+      Google Maps API Key is missing!
+    </v-alert>
     <v-card>
       <GoogleMap
         :api-key="apiKey"
@@ -67,24 +72,25 @@ watchEffect(() => {
         :region="region"
         :center="center"
         :zoom="zoom"
-        style="width: 100%; height: 500px"
         ref="mapRef"
         class="map-block__google-map"
-        :options="{ disableDefaultUI: true }"
-      >
-        <AdvancedMarker class="advanced-marker" :options="{ position: markerOptions.position }">
+        :options="{ disableDefaultUI: true }">
+        <AdvancedMarker
+          class="advanced-marker"
+          :options="{ position: markerOptions.position }">
           <template #content>
             <div class="dent-life-sign">
               <v-img
                 class="dent-life-sign__image"
                 :src="userIconsMap['logo-white.svg']"
                 max-width="14"
-                alt="logo"
-              />
+                alt="logo" />
               <span class="dent-life-sign__text">{{ markerOptions.title }}</span>
             </div>
           </template>
-          <InfoWindow v-model="infoWindow" class="advanced-marker__info">
+          <InfoWindow
+            v-model="infoWindow"
+            class="advanced-marker__info">
             <ContactsBlock />
           </InfoWindow>
         </AdvancedMarker>
@@ -93,82 +99,98 @@ watchEffect(() => {
   </div>
 </template>
 <style lang="scss" scoped>
-.map-block {
-  .advanced-marker {
-    .dent-life-sign {
-      $sign-color: rgb(var(--v-theme-secondary));
+  @use '@/styles/settings.scss';
+  .map-block {
+    &__google-map {
       width: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      gap: 8px;
-      background-color: $sign-color;
-      padding: 8px 12px;
-      border-radius: 4px;
-      position: relative;
-      &::before {
-        content: '';
-        position: absolute;
-        width: 8px;
-        height: 8px;
-        border-radius: 50%;
-        background-color: $sign-color;
-        bottom: -8px;
-        left: 50%;
-        transform: translateX(-50%);
-      }
-      &__image {
-        width: 100%;
-        flex-shrink: 0;
-      }
-      &__text {
-        font-size: 0.75rem;
-        flex-shrink: 0;
-        letter-spacing: 1.5px;
-        font-weight: 500;
-        color: $white;
-        line-height: normal;
-      }
-    }
-  }
-}
-</style>
-<style lang="scss">
-.map-block {
-  &__google-map {
-    .gm-style-iw.gm-style-iw-c {
-      min-width: 295px !important;
-    }
-    .gm-style-iw-chr {
-      position: relative;
-    }
-    .gm-style-iw-ch {
-      display: none;
-    }
-    .gm-ui-hover-effect {
-      position: absolute !important;
-      right: 0;
-      width: 32px !important;
-      height: 32px !important;
-      display: grid !important;
-      place-content: center;
-      & > span {
-        margin: 0 !important;
-        width: 20px !important;
-        height: 20px !important;
-      }
+      height: 500px;
     }
     .advanced-marker {
-      &__info {
-        padding-top: 24px;
+      .dent-life-sign {
+        $sign-color: rgb(var(--v-theme-secondary));
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 8px;
+        background-color: $sign-color;
+        padding: 8px 12px;
+        border-radius: 4px;
+        position: relative;
+        &::before {
+          content: '';
+          position: absolute;
+          width: 8px;
+          height: 8px;
+          border-radius: 50%;
+          background-color: $sign-color;
+          bottom: -8px;
+          left: 50%;
+          transform: translateX(-50%);
+        }
+        &__image {
+          width: 100%;
+          flex-shrink: 0;
+        }
+        &__text {
+          font-size: 0.75rem;
+          flex-shrink: 0;
+          letter-spacing: 1.5px;
+          font-weight: 500;
+          color: $white;
+          line-height: normal;
+        }
       }
     }
-    // removes Pegman in the bottom right corner, brutal way
-    // .gmnoprint.gm-bundled-control.gm-bundled-control-on-bottom {
-    //   .gm-svpc {
-    //     display: none !important;
-    //   }
-    // }
   }
-}
+</style>
+<style lang="scss">
+  @use '@/styles/settings.scss';
+
+  .map-block {
+    &__google-map {
+      .gm-style-iw.gm-style-iw-c {
+        min-width: 295px !important;
+      }
+      .gm-style-iw-chr {
+        position: relative;
+      }
+      .gm-style-iw-ch {
+        display: none;
+      }
+      .gm-style-iw-d {
+        // padding-right: 12px !important;
+        // padding-inline-end: 12px !important;
+        overflow: hidden !important;
+        padding: 0 12px 12px 0 !important;
+      }
+      .gm-ui-hover-effect {
+        position: absolute !important;
+        top: 4px;
+        right: 4px;
+        width: 32px !important;
+        height: 32px !important;
+        display: grid !important;
+        place-content: center;
+        border-radius: 50% !important;
+        background: rgba(0, 0, 0, 0.2) !important;
+        & > span {
+          margin: 0 !important;
+          width: 20px !important;
+          height: 20px !important;
+        }
+      }
+      .advanced-marker {
+        &__info {
+          padding-top: 24px;
+        }
+      }
+      // removes Pegman in the bottom right corner, brutal way
+      // .gmnoprint.gm-bundled-control.gm-bundled-control-on-bottom {
+      //   .gm-svpc {
+      //     display: none !important;
+      //   }
+      // }
+    }
+  }
 </style>
