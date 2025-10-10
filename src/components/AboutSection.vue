55fb348adf59cc41635e54e4fb13161fa282905b
Твоё имя < email  GitHub>
2025-10-10 15:41:58 +0300

    MAIN section about, photo replaced with slider

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
   import { computed } from 'vue';
   import { useMaineStore } from '@/stores/main.store.ts';
-  import { createAssetMap } from '@/utils/assets.ts';
+  import { useSwiper } from '@/global/swiper/useSwiperScroll.js';
 
   const store = useMaineStore();
+  const { handleProgress, gradientOpacity } = useSwiper();
 
   interface Props {
     options?: Record<string, unknown>;
@@ -13,10 +14,9 @@
     options: () => ({})
   });
 
-  const pictures = import.meta.glob<{ default: string }>('@/assets/images/*', {
+  const slides = import.meta.glob<{ default: string }>('@/assets/images/interior/*', {
     eager: true
   });
-  const picturesMap = createAssetMap(pictures);
 
   const defaultOptions: Record<string, unknown> = {};
 
@@ -47,16 +47,47 @@
     </div>
     <div class="section__wrapper">
       <div class="section__left-side">
-        <!-- <div style="display: block; min-height: 400px; aspect-ratio: 13/9; margin-right: auto"> -->
-        <v-img
-          class="section__image"
-          v-if="picturesMap['patient.jpg']"
-          :src="picturesMap['patient.jpg']"
-          aspect-ratio="4/3"
-          alt="about-dent-life"
-          min-width="560"
-          cover />
-        <!-- </div> -->
+        <swiper-container
+          v-if="Object.values(slides).length > 0"
+          class="swiper-horizontal"
+          ref="swiperEl"
+          @swiperprogress="handleProgress"
+          direction="horizontal"
+          slides-per-view="auto"
+          :breakpoints="{
+            '1': {
+              slidesPerView: 1,
+              freeMode: { enabled: false }
+            },
+            '630': {
+              slidesPerView: 'auto',
+              freeMode: { enabled: true, momentum: false, momentumBounce: false }
+            }
+          }"
+          free-mode
+          :free-mode-momentum="false"
+          :free-mode-momentum-bounce="false"
+          :scrollbar="{ draggable: true, dragSize: 150 }"
+          :navigation="true"
+          :space-between="0"
+          observer
+          observe-slide-children
+          touch-start-force-prevent-default>
+          <swiper-slide
+            v-for="(slide, idx, id) in slides"
+            :key="`slide-key-${idx}`"
+            class="swiper-horizontal__slide">
+            <v-img
+              v-if="slide.default"
+              :class="`photo-${id}`"
+              :src="slide.default"
+              :key="idx"
+              absolute
+              cover
+              position="center"
+              alt="Dent-Life" />
+          </swiper-slide>
+        </swiper-container>
       </div>
       <div class="section__right-side">
         <div v-if="mainOptions.items?.length" class="list">
@@ -84,6 +115,34 @@
 
 <style lang="scss" scoped>
   @use '@/styles/settings.scss';
+  .section-header {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    padding: 24px;
+    background: $primary-lighten5;
+    @include settings.respond-down(sm) {
+      padding: 24px 12px 32px 12px;
+    }
+    &__icon {
+      padding: 8px;
+      border-radius: 8px;
+      border: 2px solid rgba($dent-icon, 0.5);
+      min-width: 64px;
+      height: 64px;
+      background-color: $white;
+    }
+    &__title {
+      padding: 0 24px;
+      color: $black-lighten1;
+      // text-shadow: 0 0 8px rgba(0, 0, 0, 0.8);
+    }
+    &__sub-title {
+      padding: 0 24px;
+      font-weight: 500;
+      color: $black-lighten2;
+    }
+  }
   .section {
     min-height: 400px;
     padding: 16px;
@@ -91,9 +150,10 @@
       display: flex;
       flex-wrap: wrap;
       justify-content: center;
-      gap: 64px 24px;
+      gap: 32px 24px;
       padding: 24px 12px;
       @include settings.respond-down(sm) {
+        display: block;
         padding: 0;
       }
     }
@@ -107,59 +167,24 @@
       height: 100%;
       background: rgba($white, 0.2);
     }
-    // &__left-side {
-    // }
-    // &__right-side {
-    // }
+    // &__left-side {}
+    // &__right-side {}
   }
-  .section-header {
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-between;
-    padding: 24px;
-    background: #dceaea;
-    @include settings.respond-down(sm) {
-      padding: 24px 12px 32px 12px;
-    }
-    &__icon {
-      padding: 8px;
-      border-radius: 8px;
-      border: 2px solid rgba($dent-icon, 0.5);
-      min-width: 64px;
-      height: 64px;
-      background-color: #fff;
-    }
-    &__title {
-      padding: 0 24px;
-      color: $black-lighten1;
-      // text-shadow: 0 0 8px rgba(0, 0, 0, 0.8);
-    }
-    &__sub-title {
-      padding: 0 24px;
-      font-weight: 500;
-      color: $black-lighten2;
-    }
-  }
+
   .list {
     height: 100%;
+    max-height: 410px;
     max-width: 600px;
     display: flex;
     flex-direction: column;
     gap: 12px;
-    // border-radius: 32px 32px 0 0;
     background: $primary-lighten6;
     padding: 24px;
     @include settings.respond-down(lg) {
       max-width: unset;
+      max-height: unset;
       border-radius: 0;
-      // outline: 3px solid greenyellow;
     }
-    // @include settings.respond-down(sm) {
-    //   border-radius: 0;
-    //   margin: 0;
-    // }
-    // &__item {
-    // }
     &__item-wrapper {
       display: flex;
       align-items: center;
@@ -174,15 +199,48 @@
       width: 100%;
       flex-shrink: 0;
     }
-    &__item-title {
-      // text-transform: uppercase;
-    }
+    // &__item-title {}
     &__item-icon {
       flex-shrink: 0;
       width: 100%;
       max-width: 24px;
       margin-left: auto;
       align-self: baseline;
+    }
+  }
+  .swiper-horizontal {
+    max-width: 540px;
+    @include settings.respond-down(sm) {
+      margin-bottom: 32px;
+    }
+    &__slide {
+      // width: 100% !important;
+      // min-width: 240px;
+      height: 408px;
+      aspect-ratio: 4/3;
+      position: relative;
+      box-shadow: 0 4px 16px rgba(0, 0, 0, 0.25);
+      @include settings.respond-down(sm) {
+        height: 350px;
+      }
+    }
+  }
+  swiper-container.swiper-horizontal::part(container) {
+    width: 100%;
+  }
+  // NAV Buttons
+  swiper-container.swiper-horizontal::part(button-prev) {
+    opacity: v-bind('gradientOpacity.start');
+  }
+  swiper-container.swiper-horizontal::part(button-next) {
+    opacity: v-bind('gradientOpacity.end');
+  }
+  swiper-container.swiper-horizontal::part(button-prev),
+  swiper-container.swiper-horizontal::part(button-next) {
+    top: calc(50% - 30px / 2);
+    background: rgba($black, 0.3);
+    @include settings.respond-down(sm) {
+      border: none;
     }
   }
 </style>
